967c7b75d0960ebe7bf97325946e8b7b3f19fe53
Julia-Gareeva <uliastekolnikova12822@gmail.com>
2022-12-20 11:56:57 +0500

    Изменила ads и все файлы внутри. Добавила новую директорию users. Также добавила директорию data.

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from ads.models import Ad, Category, Location
 
-# Register your models here.
+admin.site.register(Ad)
+admin.site.register(Category)
+admin.site.register(Location)
